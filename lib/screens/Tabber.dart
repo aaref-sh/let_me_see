e720246fa415a2f61ce25adbcfc78459c948ad99
aaref-sh <m.aaref-sh@hotmail.com>
2021-03-17 19:47:10 +0200

    add and imlement upload

--- lib/screens/Tabber.dart
+++ lib/screens/Tabber.dart
@@ -1,10 +1,17 @@
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import 'package:let_me_see/model/model.dart';
 import 'package:let_me_see/screens/Home.dart';
 import 'package:let_me_see/screens/Notifications.dart';
 import 'package:let_me_see/screens/TableScreen.dart';
 import 'package:line_icons/line_icons.dart';
+import 'package:path/path.dart';
+import 'package:async/async.dart';
+import 'dart:convert';
 
 List<Lecture> lecturelist;
 List<Notificate> notificationlist = <Notificate>[];
@@ -22,10 +29,7 @@ class _TabberState extends State<Tabber> {
     HomeScreen(),
     TableScreen(),
     Notifications(),
-    Text(
-      'Likes',
-      style: optionStyle,
-    ),
+    PickFile(),
   ];
   @override
   Widget build(BuildContext context) {
@@ -79,4 +83,61 @@ class _TabberState extends State<Tabber> {
       ),
     );
   }
+}
+
+class PickFile extends StatefulWidget {
+  @override
+  _PickFileState createState() => _PickFileState();
+}
+
+class _PickFileState extends State<PickFile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: TextButton(
+          child: Text('اختر ملف'),
+          onPressed: () async {
+            FilePickerResult result =
+                await FilePicker.platform.pickFiles(allowMultiple: true);
+            if (result != null) {
+              List<File> files =
+                  result.paths.map((path) => File(path)).toList();
+              for (var i in files) {
+                print(i.path.split("/").last);
+                uploadFile(i);
+              }
+            } else {
+              // User canceled the picker
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+uploadFile(File f) async {
+  // var postUri = Uri.parse("apiUrl");
+  // http.MultipartRequest request = new http.MultipartRequest("POST", postUri);
+  // http.MultipartFile multipartFile =
+  //     await http.MultipartFile.fromPath(f.path.split("/").last, f.path);
+  // request.files.add(multipartFile);
+  // http.StreamedResponse response = await request.send();
+  // print(response.statusCode);
+  var request = http.MultipartRequest(
+      'POST', Uri.parse('http://192.168.1.111:66/api/values/ups'));
+  request.files.add(await http.MultipartFile.fromPath('docc', f.path));
+  var res = await request.send();
+  print(res.reasonPhrase);
+  // var request = new http.MultipartRequest("POST", Uri.parse(''));
+  // // request.fields['user'] = 'someone@somewhere.com';
+  // request.files.add(await http.MultipartFile.fromPath(
+  //   f.path.split('/').last,
+  //   f.path,
+  //   contentType: MediaType('application', 'pdf'),
+  // ));
+  // request.send().then((response) {
+  //   if (response.statusCode == 200) print("Uploaded!");
+  // });
 }
